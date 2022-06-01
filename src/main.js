a1b7277483a3f8d2519710be95e6a18d31a36712
wwhtml <1129942397@qq.com>
2022-06-01 21:49:50 +0800

    ant、normalize

--- src/main.js
+++ src/main.js
@@ -3,4 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+//styles
+import "@/styles/main.scss"
+
+//ant-design-vue
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+
+
+createApp(App).use(store).use(router).use(Antd).mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,31 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+    // lintOnSave: process.env.NODE_ENV !== "production",
+    lintOnSave: false,
+    devServer: {
+      // open: true,
+      host: "localhost",
+      port: 8888,
+      proxy: {
+        [process.env.VUE_APP_ACCOUNT_FLAG]: {
+          target: process.env.VUE_APP_ACCOUNT_APIURL,
+          ws: false, // webstock
+          changeOrigin: true, // 是否开启跨域
+          pathRewrite: { [`^${process.env.VUE_APP_ACCOUNT_FLAG}`]: "" }
+        },
+        [process.env.VUE_APP_USER_FLAG]: {
+          target: process.env.VUE_APP_USER_APIURL,
+          ws: false, // webstock
+          changeOrigin: true, // 是否开启跨域
+          pathRewrite: { [`^${process.env.VUE_APP_USER_FLAG}`]: "" }
+        },
+        [process.env.VUE_APP_ROLE_FLAG]: {
+          target: process.env.VUE_APP_ROLE_APIURL,
+          ws: false, // webstock
+          changeOrigin: true, // 是否开启跨域
+          pathRewrite: { [`^${process.env.VUE_APP_ROLE_FLAG}`]: "" }
+        }
+      }
+    }
 })
